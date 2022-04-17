e94bd986dd85a5da63c2654bcc7d8ec4c5435a51
AshkanAhmadi <ashkan16ma96@gmail.com>
2022-04-17 15:18:34 +0430

    add empty list error to customers page

--- src/Components/Customers.js
+++ src/Components/Customers.js
@@ -4,15 +4,19 @@ const Customers = (props) => {
   return (
     <div className="customers">
       <h1>Customers List</h1>
-      {props.customers.map((item) => (
-        <div className="singleCustomer" key={item.id}>
-          <div>
-            <FaUserAlt className="icon" />
-            <h3>{item.name}</h3>
+      {props.customers.length == 0 ? (
+        <p className="empty_list">No customers registered</p>
+      ) : (
+        props.customers.map((item) => (
+          <div className="singleCustomer" key={item.id}>
+            <div>
+              <FaUserAlt className="icon" />
+              <h3>{item.name}</h3>
+            </div>
+            <span>{item.city}</span>
           </div>
-          <span>{item.city}</span>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
